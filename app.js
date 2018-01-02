681602bd67cfb74c3fbd382aca13cc6dd66cf7fb
ThadeusAjayi <ainojie@gmail.com>
2018-01-02 22:50:19 +0100

    Working code with io connection in www file

--- app.js
+++ app.js
@@ -4,15 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var socketio = require('socket.io');
-var http = require("http");
+var app = express();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
-var app = express();
-var server = http.createServer(app);
-var io = socketio.listen(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,13 +21,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
-
 app.use('/', index);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
