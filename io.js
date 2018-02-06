75fd8c00882eecfd7ee78cbdc26dc03859c5f807
ThadeusAjayi <ainojie@gmail.com>
2018-02-06 11:46:01 +0100

    Log chat from device

--- io.js
+++ io.js
@@ -24,6 +24,7 @@ io.on('connection', function (socket) {
     /* Chat creation is done from where all the mentors are listed somewhere different */
 
     socket.on('chatMessage', (data) => {
+        console.log(data);
         var newChat = addDateToChat(data);
         console.log(newChat);
         Chat.update({ _id: currentChatId }, { $addToSet: { "chatmessages": newChat } }, (err, cm) => {
